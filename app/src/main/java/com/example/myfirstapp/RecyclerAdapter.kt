e41426eea212d80c13c5eba2f41f121c5a156dc8
Klimptoon <tim185032@gmail.com>
2022-02-26 21:42:57 +0300

    Layouts and a bit of code

---
+++ app/src/main/java/com/example/myfirstapp/RecyclerAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.myfirstapp
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myfirstapp.databinding.RecyclerItemBinding
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder> {
+
+    class RecyclerHolder(item : View) : RecyclerView.ViewHolder(item) {
+        val binding = RecyclerItemBinding.bind(item)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
