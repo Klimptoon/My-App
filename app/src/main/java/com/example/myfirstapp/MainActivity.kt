be093ac21f6526cd6fddcb26798614bd132a271c
Klimptoon <tim185032@gmail.com>
2022-03-04 19:04:55 +0300

    alert dialog + recycler are working

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -6,9 +6,7 @@ import android.os.Bundle
 import android.view.WindowManager
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myfirstapp.databinding.ActivityMainBinding
 import com.example.myfirstapp.databinding.PurchaseInputBinding
 
@@ -16,45 +14,38 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMainBinding
 
-    private var binding2 = null
+
+    var imageId : Int = 0
+    var type : String = ""
+    var title : String = ""
+    var costPurchase: String = ""
+
 
 
 
     private val adapter = RecyclerAdapter()
-    private val imageIdList = listOf(R.drawable.circle_shape_red
-        , R.drawable.circle_shape_blue
-        , R.drawable.circle_shape_green
-        , R.drawable.circle_shape_yellow
-    )
+//    private val imageIdList = listOf(R.drawable.circle_shape_red
+//        , R.drawable.circle_shape_blue
+//        , R.drawable.circle_shape_green
+//        , R.drawable.circle_shape_yellow
+//    )
     private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.buttonAdd.setOnClickListener {
             showAlertDialog()
-
         }
+
     }
 
 
 
-
-
-
-
     private fun start() {
-        binding.apply {
-            rv.layoutManager = GridLayoutManager(this@MainActivity, 1)
-            rv.adapter = adapter
-            buttonAdd.setOnClickListener {
-                if(counter > 3) counter = 0
-                val purchase = Purchase(imageIdList[counter], "Овощь", "Помидор", 12.0)
-                adapter.addPurchase(purchase)
-                counter++
-            }
-        }
+
     }
 
     private fun showAlertDialog() {
@@ -73,7 +64,7 @@ class MainActivity : AppCompatActivity() {
             dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
                 val enteredText = dialogBinding.etName.text.toString()
                 val cost = dialogBinding.etCost.text.toString()
-                val enteredText2 = cost.toIntOrNull()
+                val enteredText2 = cost.toDoubleOrNull()
                 if(enteredText2 == null || enteredText2 < 0) {
                     dialogBinding.etCost.error = getString(R.string.empty)
                     return@setOnClickListener
@@ -82,12 +73,29 @@ class MainActivity : AppCompatActivity() {
                     dialogBinding.etName.error = getString(R.string.empty)
                     return@setOnClickListener
                 }
-                this.binding.textView.text = enteredText
+                type = dialogBinding.autoCompleteTextView.text.toString()
+                title = dialogBinding.etName.text.toString()
+                costPurchase = dialogBinding.etCost.text.toString()
                 dialog.dismiss()
+                binding.apply {
+                    rv.layoutManager = GridLayoutManager(this@MainActivity, 1)
+                    rv.adapter = adapter
+                        if(counter > 3) counter = 0
+                        val purchase = Purchase(when(type) {
+                            "Еда" -> R.drawable.circle_shape_red
+                            "Одежда" -> R.drawable.circle_shape_blue
+                            "Развлечения" -> R.drawable.circle_shape_green
+                            "Дом" -> R.drawable.circle_shape_yellow
+                            else -> R.drawable.circle_shape_blue
+                                                           }, type, title, costPurchase)
+                        adapter.addPurchase(purchase)
+                        counter++
+                }
             }
 
         }
         dialog.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
         dialog.show()
+
     }
 }
